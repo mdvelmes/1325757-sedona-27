612450130cc1357b13d285f6470bc4f836ade1ea
mdvelmes <60127354+mdvelmes@users.noreply.github.com>
2020-03-08 01:48:32 +0300

    джава модального

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// var button = document.querySelector(".search-button");
+// var popup = document.querySelector(".modal");
+
+// button.addEventListener("click", function(evt) {
+//     evt.preventDefault();
+
+//     if (popup.classList.contains('modal-visible')) {
+//         popup.classList.remove("modal-visible")
+//     } else {
+//         popup.classList.add("modal-visible")
+//     }
+// });
+
+var button = document.querySelector(".search-button");
+var popup = document.querySelector(".modal");
+
+var form = popup.querySelector("form");
+var arrivalDate = popup.querySelector("[name=arrival-date]");
+var departureDate = popup.querySelector("[name=departure-date]");
+var adults = popup.querySelector("[name=adults]");
+var kids = popup.querySelector("[name=kids]");
+
+button.addEventListener("click", function(evt) {
+    evt.preventDefault();
+
+    popup.classList.toggle("modal-visible");
+    popup.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function(evt) {
+    if (!arrivalDate.value || !departureDate.value) {
+        evt.preventDefault();
+
+        popup.classList.remove("modal-error");
+        popup.offsetWidth = popup.offsetWidth;
+        popup.classList.add("modal-error");
+    } else {
+        console.log("fuckOFF");
+    }
+});
